a3359e0e1493cab2ae83e075b390d7534d246d56
Matias-Barrios <soymatiasbarrios@gmail.com>
2018-04-19 19:53:31 -0300

    mas

--- select_simple.sql
+++ select_simple.sql
@@ -1,6 +1,8 @@
 # command : dbaccess - select_simple.sql
 CONNECT TO 'gestion_utu@miServidor' USER 'matias' USING 'PASSWORDHERE';
 
+USE gestion_utu;
+
 CREATE TABLE IF NOT EXISTS Usuarios 
  (
   id_usuario  SERIAL PRIMARY KEY,
