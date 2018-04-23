abaade3cf6b55df77b69a1b4726cb4660ca6ab3c
Matias.Barrios <30472800+Matias-Barrios@users.noreply.github.com>
2018-04-23 11:03:25 -0500

    Update ingresar_usuarios.sql

--- CrearDB/ingresar_usuarios.sql
+++ CrearDB/ingresar_usuarios.sql
@@ -4,22 +4,6 @@
 CONNECT TO 'gestion_utu@miServidor' USER 'admin_proyecto'  USING 'XXXXXXXX';
 
 INSERT INTO Usuarios (CI, primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,grado,fecha_nacimiento,nota,email,hace_proyecto,tipo,encriptacion_hash,encriptacion_sal,baja)
-VALUES (
-        41217832,
-        "Gabriel"
-        "Matias",
-        "Barrios",
-        "Cabrera",
-        NULL,
-        "10/28/1986",
-        1,
-        "soymatiasbarrios@gmail.com"
-        true,
-        "Alumno",
-        NULL,
-        NULL,
-        false
-        );
-
+VALUES (41217832,"Gabriel","Matias","Barrios","Cabrera",1,"10/28/1986",1,"soymatiasbarrios@gmail.com","t","Alumno",NULL,NULL,"f");
 
  
