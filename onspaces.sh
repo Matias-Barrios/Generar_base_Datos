979607ddff17695858dbb294c74f0c6e0189c2a4
Matias.Barrios <30472800+Matias-Barrios@users.noreply.github.com>
2018-04-23 09:35:52 -0500

    Create onspaces.sh

---
+++ onspaces.sh
@@ -0,0 +1,2 @@
+onspaces -c -d proyectoUTUinformix -p /matiasInformixDBSpaces/dbspace_gestionutu -o 0 -s 100000
+onspaces -c -d dbspace_matias -p /matiasInformixDBSpaces/dbspace_matias -o 0 -s 100000
