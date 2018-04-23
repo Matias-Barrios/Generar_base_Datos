8873647d759bd5a278047b7837af367eed0e68f7
Matias.Barrios <30472800+Matias-Barrios@users.noreply.github.com>
2018-04-23 10:12:48 -0500

    Create ingresar_usuarios.sql

---
+++ CrearDB/ingresar_usuarios.sql
@@ -0,0 +1,25 @@
+-- command : dbaccess - select_simple.sql
+-- CREATE TABLE IF NOT EXISTS Usuarios
+
+CONNECT TO 'gestion_utu@miServidor' USER 'admin_proyecto'  USING 'XXXXXXXX';
+
+INSERT INTO Usuarios (CI, primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,grado,fecha_nacimiento,nota,email,hace_proyecto,tipo,encriptacion_hash,encriptacion_sal,baja)
+VALUES (
+        41217832,
+        "Gabriel"
+        "Matias",
+        "Barrios",
+        "Cabrera",
+        NULL,
+        "10/28/1986",
+        1,
+        "soymatiasbarrios@gmail.com"
+        true,
+        "Alumno",
+        NULL,
+        NULL,
+        false
+        );
+
+
+ 
