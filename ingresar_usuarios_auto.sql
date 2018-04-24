75b9dc323b59befff13ff197a718ef20e16d9acd
Matias-Barrios <soymatiasbarrios@gmail.com>
2018-04-24 14:59:06 +0000

    mas

---
+++ ingresar_usuarios_auto.sql
@@ -0,0 +1,37 @@
+CONNECT TO 'gestion_utu@miServidor' USER 'admin_proyecto'  USING 'XXXXPASSWORDXXXX';
+
+
+INSERT INTO Usuarios (CI, primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,grado,fecha_nacimiento,nota,email,hace_proyecto,tipo,encriptacion_hash,encriptacion_sal,baja)
+VALUES ( 35097076 , "Ester" , "Esiquio" , "Serrano" , "Castro" , 3, "11/15/1993" , 1 , "Ester.Esiquio.Serrano.Castro@hotmail.com" , "f" , "Admin" , NULL , NULL , "f" );
+
+
+
+
+INSERT INTO Usuarios (CI, primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,grado,fecha_nacimiento,nota,email,hace_proyecto,tipo,encriptacion_hash,encriptacion_sal,baja)
+VALUES ( 33769641 , "César" , "Cleofás" , "Herrera" , "Soto" , 5, "09/08/1975" , 1 , "César.Cleofás.Herrera.Soto@hotmail.com" , "f" , "Admin" , NULL , NULL , "f" );
+
+
+
+
+INSERT INTO Usuarios (CI, primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,grado,fecha_nacimiento,nota,email,hace_proyecto,tipo,encriptacion_hash,encriptacion_sal,baja)
+VALUES ( 29182968 , "Martín" , "Onésimo" , "Vega" , "Medina" , 5, "02/25/1999" , 1 , "Martín.Onésimo.Vega.Medina@hotmail.com" , "f" , "Admin" , NULL , NULL , "f" );
+
+
+
+
+INSERT INTO Usuarios (CI, primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,grado,fecha_nacimiento,nota,email,hace_proyecto,tipo,encriptacion_hash,encriptacion_sal,baja)
+VALUES ( 34327199 , "Heraclio" , "Teófanes" , "Cruz" , "Cruz" , 5, "02/26/1994" , 1 , "Heraclio.Teófanes.Cruz.Cruz@hotmail.com" , "f" , "Admin" , NULL , NULL , "f" );
+
+
+
+
+INSERT INTO Usuarios (CI, primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,grado,fecha_nacimiento,nota,email,hace_proyecto,tipo,encriptacion_hash,encriptacion_sal,baja)
+VALUES ( 33431854 , "Albina" , "Poncio" , "Parra" , "Cruz" , 5, "10/09/1971" , 1 , "Albina.Poncio.Parra.Cruz@hotmail.com" , "f" , "Admin" , NULL , NULL , "f" );
+
+
+
+
+INSERT INTO Usuarios (CI, primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,grado,fecha_nacimiento,nota,email,hace_proyecto,tipo,encriptacion_hash,encriptacion_sal,baja)
+VALUES ( 24628157 , "Gloria" , "Rodrigo" , "Ramos" , "Márquez" , 2, "03/10/1970" , 1 , "Gloria.Rodrigo.Ramos.Márquez@hotmail.com" , "f" , "Admin" , NULL , NULL , "f" );
+
+
