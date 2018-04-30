85dc0801d6206e7f265290cf45a2366079fa8889
Matias.Barrios <30472800+Matias-Barrios@users.noreply.github.com>
2018-04-30 14:28:41 -0500

    Update ingresar_institutos.sql

--- CrearDB/ingresar_institutos.sql
+++ CrearDB/ingresar_institutos.sql
@@ -5,4 +5,49 @@ CONNECT TO 'gestion_utu@miServidor' USER 'admin_proyecto'  USING 'XXXXXXXX';
 INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja)
 VALUES ('Escuela Técnica "Arroyo Seco"', "Av. Agraciada Esq. Aguilar", 2544, "29243865|29243856", "etas010@gmail.com", "f");
 
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja)
+VALUES ('Intituto Técnico Superior "Buceo"', "Av. Rivera Esq. Tomas de Tésanos", 3729 , "26285408|26285813", "itsbuceo@gmail.com", "f");
 
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja)
+VALUES ('Escuela Técnica Cerro "Mtro. Nicasio García"', "Portugal Esq. Carlos Mª Ramírez", 4257 , "23111056|23119407|23114949", "portugal4257@hotmail.com", "f");
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja)
+VALUES ('Escuela Técnica Colón  "Don Alberico Passadore"', "Cno. Colman Esq. Cesar Mayo Gutierrez", 5274 , "23209511|23205789", "estecolon@gmail.com", "f");
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja)
+VALUES ('Escuela Técnica Flor de Maroñas', "Andrés Latorre Esq. Veracierto", 4914 , "25148177|25148210", "utuflor2012@hotmail.com", "f");
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja)
+VALUES ('Escuela Técnica Artigas', "Bernabé Rivera Esq. Gral Rivera", 626 , "47723687|47725988", "etautu626@adinet.com.uy", "f");
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja)
+VALUES ('Escuela Técnica de Atlántida', "Calle Republica Argentina Entre Republica de Chile y Gral. Artigas", , "43723507|43728578", "utuatlantida@adinet.com.uy", "f");
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja)
+VALUES ('Escuela Técnica Las Piedras', "Baltasar Brum Esq. Pilar Cabrera", 665, "23644564|23641741|23649849", "escuelalaspiedras2011@gmail.com", "f");
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja)
+VALUES ('Escuela Técnica San Ramón "Juan Pedro Tapie"', "José Batlle Y Ordóñez Esq. Rivera", 1698, "43122409|43123593", "utujpt@adinet.com.uy", "f");
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja)
+VALUES ('Escuela Técnica Fraile Muerto', "Julio María Cremona s/n Esq. 25 de agosto.(Fraile Muerto)", , "46889031", "escuelatecnicafrailemuertocl@gmail.com", "f");
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja)
+VALUES ('Escuela Técnica Melo', "Treinta y Tres Esq. Oribe (Melo)", 290, "46422121|46429133|46424496", "escuelatmelo@gmail.com", "f");
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja)
+VALUES ('Escuela Técnica Carmelo', "José E. Rodó Esq. Ruta 21", , "45422317|45424015", "utucarmelo@hotmail.com", "f");
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja)
+VALUES ('Escuela Técnica ERCNA', "18 de Julio s/n esq. Arroyo de las Vacas (Carmelo)", , "45422372|45423912", "utu_ercna@hotmail.com", "f");
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja)
+VALUES ('Escuela Técnica Colonia', "F.D. Roosvelt esq. Río Negro", 167, "45222960|45220261", "utu.colonia.sac@gmail.com", "f");
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja)
+VALUES ('Escuela Técnica Durazno', "F.D. Roosvelt esq. Río Negro", 167, "43626288|43627735", "etecnicadurazno@gmail.com", "f");
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja)
+VALUES ('Escuela Técnica Sarandí del Yí', "A. Sosa Bernadet Esq. Alvariza", 579, "43679068|43639099", "utusarandidelyi@gmail.com", "f");
+
+-- fin
