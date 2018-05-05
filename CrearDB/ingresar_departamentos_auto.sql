29b23f2222317b18cbe86ff57c889d12b64a0f08
Matias.Barrios <30472800+Matias-Barrios@users.noreply.github.com>
2018-05-05 17:03:37 -0300

    Create ingresar_departamentos_auto.sql

---
+++ CrearDB/ingresar_departamentos_auto.sql
@@ -0,0 +1,61 @@
+-- command : dbaccess - select_simple.sql
+
+CONNECT TO 'gestion_utu@miServidor' USER 'XXXNOMBREUSUARIOXXX' USING 'XXXPASSWORDXXX';
+
+INSERT INTO Departamentos (nombre_departamento, baja)
+VALUES ('Artigas', "f");
+
+INSERT INTO Departamentos (nombre_departamento, baja)
+VALUES ('Canelones', "f");
+
+INSERT INTO Departamentos (nombre_departamento, baja)
+VALUES ('Cerro Largo', "f");
+
+INSERT INTO Departamentos (nombre_departamento, baja)
+VALUES ('Colonia', "f");
+
+INSERT INTO Departamentos (nombre_departamento, baja)
+VALUES ('Durazno', "f");
+
+INSERT INTO Departamentos (nombre_departamento, baja)
+VALUES ('Flores', "f");
+
+INSERT INTO Departamentos (nombre_departamento, baja)
+VALUES ('Florida', "f");
+
+INSERT INTO Departamentos (nombre_departamento, baja)
+VALUES ('Lavalleja', "f");
+
+INSERT INTO Departamentos (nombre_departamento, baja)
+VALUES ('Maldonado', "f");
+
+INSERT INTO Departamentos (nombre_departamento, baja)
+VALUES ('Montevideo', "f");
+
+INSERT INTO Departamentos (nombre_departamento, baja)
+VALUES ('Paysandú', "f");
+
+INSERT INTO Departamentos (nombre_departamento, baja)
+VALUES ('Río Negro', "f");
+
+INSERT INTO Departamentos (nombre_departamento, baja)
+VALUES ('Rivera', "f");
+
+INSERT INTO Departamentos (nombre_departamento, baja)
+VALUES ('Rocha', "f");
+
+INSERT INTO Departamentos (nombre_departamento, baja)
+VALUES ('Salto', "f");
+
+INSERT INTO Departamentos (nombre_departamento, baja)
+VALUES ('San José', "f");
+
+INSERT INTO Departamentos (nombre_departamento, baja)
+VALUES ('Soriano', "f");
+
+INSERT INTO Departamentos (nombre_departamento, baja)
+VALUES ('Tacuarembó', "f");
+
+INSERT INTO Departamentos (nombre_departamento, baja)
+VALUES ('Treinta y Tres', "f");
+
