2185648d9dd0683d20e6cca01e28c484c74b25d0
Matias.Barrios <30472800+Matias-Barrios@users.noreply.github.com>
2018-05-06 00:31:41 -0300

    Create ejecutar_todo.sh

---
+++ CrearDB/ejecutar_todo.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+echo "Ejecutar todo - $( date )"
+
+dbaccess - Generar_estructuras.sql && \
+dbaccess - ingresar_alumnos_auto.sql && \
+dbaccess - ingresar_docentes_auto.sql && \
+dbaccess - ingresar_departamentos_auto.sql && \
+dbaccess - ingresar_institutos_auto.sql && \
+dbaccess - ingresar_grupos_auto.sql
+
+echo "Completado!! "`date`
+
+
+	
