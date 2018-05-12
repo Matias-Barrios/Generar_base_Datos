ca85e80c831eedb4dafe11cc309afbfe3fb88170
Matias.Barrios <30472800+Matias-Barrios@users.noreply.github.com>
2018-05-12 11:35:20 -0300

    Update LEO_ingresar_relacion_Profesor_pertenece_Instituto_auto.sql

--- CrearDB/LEO_ingresar_relacion_Profesor_pertenece_Instituto_auto.sql
+++ CrearDB/LEO_ingresar_relacion_Profesor_pertenece_Instituto_auto.sql
@@ -1,8 +1,8 @@
 CONNECT TO 'gestion_utu@miServidor' USER 'XXXNOMBREUSUSARIOXXX' USING 'XXXPASSWORDXXX';
 
-INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_id_grupo, foranea_id_instituto)
+INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_CI_profesor, foranea_id_instituto)
 VALUES ( 1 , 1);
-INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_id_grupo, foranea_id_instituto)
+INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_CI_profesor, foranea_id_instituto)
 VALUES ( 2 , 1);
-INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_id_grupo, foranea_id_instituto)
+INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_CI_profesor, foranea_id_instituto)
 VALUES ( 3 , 1);
