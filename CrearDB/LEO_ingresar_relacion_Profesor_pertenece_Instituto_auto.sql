26c78ccdf18d4d936ad7ee294bb16607dfdb3f91
Matias.Barrios <30472800+Matias-Barrios@users.noreply.github.com>
2018-05-12 00:35:18 -0300

    Create LEO_ingresar_relacion_Profesor_pertenece_Instituto_auto.sql

---
+++ CrearDB/LEO_ingresar_relacion_Profesor_pertenece_Instituto_auto.sql
@@ -0,0 +1,22 @@
+CONNECT TO 'gestion_utu@miServidor' USER 'XXXNOMBREUSUSARIOXXX' USING 'XXXPASSWORDXXX';
+
+INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_CI_profesor, foranea_id_instituto)
+VALUES (19312327, 1);
+INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_CI_profesor, foranea_id_instituto)
+VALUES (36240200, 1);
+INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_CI_profesor, foranea_id_instituto)
+VALUES (43053842, 1);
+INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_CI_profesor, foranea_id_instituto)
+VALUES (26173609, 1);
+INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_CI_profesor, foranea_id_instituto)
+VALUES (49557474, 1);
+INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_CI_profesor, foranea_id_instituto)
+VALUES (56813358, 1);
+INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_CI_profesor, foranea_id_instituto)
+VALUES (54242519, 1);
+INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_CI_profesor, foranea_id_instituto)
+VALUES (27028556, 1);
+INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_CI_profesor, foranea_id_instituto)
+VALUES (43869516, 1);
+INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_CI_profesor, foranea_id_instituto)
+VALUES (38558601, 1);
