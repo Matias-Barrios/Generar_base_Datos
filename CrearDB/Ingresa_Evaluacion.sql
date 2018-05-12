8f7111189e081b98df0aff01ce116aa2930c6ef6
proyectoCML <37819286+proyectoCML@users.noreply.github.com>
2018-05-12 11:31:49 -0300

    Update Ingresa_Evaluacion.sql

--- CrearDB/Ingresa_Evaluacion.sql
+++ CrearDB/Ingresa_Evaluacion.sql
@@ -1,1 +1,4 @@
 INSERT INTO Evaluaciones (,CI_Profesor,CI_Alumno,id_Materia,id_Grupo,Evaluaciones_nombre_vacio,categoria,fecha_eva_vacio,baja,nota) VALUES
+(43053842,57414907,1,2,'Trabajo_domiciliario','01/4/2018','f',8)
+(19312327,13839024,1,1,'Trabajo_practico','01/05/2018','f',9)
+(36240200,29729647,1,2,'Trabajo_practico,'02/05/2018','f',10)
