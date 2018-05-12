49d74d5eb78720170dfb6044c57aa6444b1042ec
Matias.Barrios <30472800+Matias-Barrios@users.noreply.github.com>
2018-05-12 00:52:17 -0300

    Create LEO_ingresar_relacion_Grupos_tienen_Materias_auto.sql

---
+++ CrearDB/LEO_ingresar_relacion_Grupos_tienen_Materias_auto.sql
@@ -0,0 +1,17 @@
+INSERT INTO relacion_Grupos_tienen_Materias (foranea_id_grupo, foranea_id_materia)
+VALUES (1, 1);
+INSERT INTO relacion_Grupos_tienen_Materias (foranea_id_grupo, foranea_id_materia)
+VALUES (1, 2);
+INSERT INTO relacion_Grupos_tienen_Materias (foranea_id_grupo, foranea_id_materia)
+VALUES (1, 3);
+INSERT INTO relacion_Grupos_tienen_Materias (foranea_id_grupo, foranea_id_materia)
+VALUES (1, 5);
+INSERT INTO relacion_Grupos_tienen_Materias (foranea_id_grupo, foranea_id_materia)
+VALUES (2, 2);
+INSERT INTO relacion_Grupos_tienen_Materias (foranea_id_grupo, foranea_id_materia)
+VALUES (2, 3);
+INSERT INTO relacion_Grupos_tienen_Materias (foranea_id_grupo, foranea_id_materia)
+VALUES (2, 4);
+INSERT INTO relacion_Grupos_tienen_Materias (foranea_id_grupo, foranea_id_materia)
+VALUES (2, 5);
+
