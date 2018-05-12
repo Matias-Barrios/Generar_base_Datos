3cd992c021916a3bb086f54bcdbd2553ef90eb9f
Matias.Barrios <30472800+Matias-Barrios@users.noreply.github.com>
2018-05-12 12:30:58 -0300

    Update LEO_ingresar_evaluaciones.sql

--- CrearDB/LEO_ingresar_evaluaciones.sql
+++ CrearDB/LEO_ingresar_evaluaciones.sql
@@ -1,9 +1,9 @@
 CONNECT TO 'gestion_utu@miServidor' USER 'XXXNOMBREUSUSARIOXXX'  USING 'XXXPASSWORDXXX';
 
-INSERT INTO Evaluaciones (CI_Profesor,CI_Alumno,id_Materia,id_Grupo,nombre_evaluacion,categoria,fecha_eva_vacio,baja,nota) VALUES
-(43053842,57414907,1,2,'Trabajo_domiciliario','01/4/2018','f',8);
-INSERT INTO Evaluaciones (CI_Profesor,CI_Alumno,id_Materia,id_Grupo,nombre_evaluacion,categoria,fecha_eva,baja,nota) VALUES
-(19312327,13839024,1,1,'Trabajo_practico','01/05/2018','f',9);
-INSERT INTO Evaluaciones (CI_Profesor,CI_Alumno,id_Materia,id_Grupo,nombre_evaluacion,categoria,fecha_eva,baja,nota) VALUES
-(36240200,29729647,1,2,'Trabajo_practico','02/05/2018','f',10);
+INSERT INTO Evaluaciones ( CI_Profesor, CI_Alumno, id_Materia, id_Grupo, nombre_evaluacion, categoria, fecha_eva, baja, nota) VALUES
+( 43053842, 57414907, 1, 2, "Traer fotos porno", 'Trabajo_domiciliario', '01/4/2018', 'f', 8);
+INSERT INTO Evaluaciones ( CI_Profesor, CI_Alumno, id_Materia, id_Grupo, nombre_evaluacion, categoria, fecha_eva, baja, nota) VALUES
+( 19312327, 13839024, 1, 1, "Traer una actriz porno", 'Trabajo_practico', '01/05/2018', 'f', 9);
+INSERT INTO Evaluaciones ( CI_Profesor, CI_Alumno, id_Materia, id_Grupo, nombre_evaluacion, categoria, fecha_eva, baja, nota) VALUES
+( 36240200, 29729647, 1, 2, "Comprar un kilo de choclos",'Trabajo_practico', '02/05/2018', 'f', 10);
 
