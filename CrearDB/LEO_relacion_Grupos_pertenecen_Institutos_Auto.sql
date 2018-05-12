e370b4987ce69eb54e914a6803ec5d8b8bde9709
Matias.Barrios <30472800+Matias-Barrios@users.noreply.github.com>
2018-05-12 00:38:16 -0300

    Create LEO_relacion_Grupos_pertenecen_Institutos_Auto.sql

---
+++ CrearDB/LEO_relacion_Grupos_pertenecen_Institutos_Auto.sql
@@ -0,0 +1,8 @@
+CONNECT TO 'gestion_utu@miServidor' USER 'XXXNOMBREUSUSARIOXXX'  USING 'XXXPASSWORDXXX';
+
+INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_id_grupo, foranea_id_instituto)
+VALUES ("3BM", 1);
+INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_id_grupo, foranea_id_instituto)
+VALUES ("3WF", 1);
+INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_id_grupo, foranea_id_instituto)
+VALUES ("3NW", 1);
