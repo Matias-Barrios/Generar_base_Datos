94adf398cd29908c1a1b849e870c3f884ec6cdf7
Matias.Barrios <30472800+Matias-Barrios@users.noreply.github.com>
2018-05-12 01:46:24 -0300

    Update LEO_relacion_Profesor_Materias_Grupos_Autos.sql

--- CrearDB/LEO_relacion_Profesor_Materias_Grupos_Autos.sql
+++ CrearDB/LEO_relacion_Profesor_Materias_Grupos_Autos.sql
@@ -1,3 +1,5 @@
+CONNECT TO 'gestion_utu@miServidor' USER 'XXXNOMBREUSUSARIOXXX'  USING 'XXXPASSWORDXXX';
+
 INSERT INTO relacion_Profesor_Materias_Grupos (foranea_CI_profesor, foranea_id_materia, foranea_id_grupo)
 VALUES (19312327 , 1 , 1 );
 INSERT INTO relacion_Profesor_Materias_Grupos (foranea_CI_profesor, foranea_id_materia, foranea_id_grupo)
