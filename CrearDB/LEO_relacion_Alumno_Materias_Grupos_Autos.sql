fd7a378121b014bc811b61aae69b2987b96b878b
Matias.Barrios <30472800+Matias-Barrios@users.noreply.github.com>
2018-05-12 01:11:59 -0300

    Create LEO_relacion_Alumno_Materias_Grupos_Autos.sql

---
+++ CrearDB/LEO_relacion_Alumno_Materias_Grupos_Autos.sql
@@ -0,0 +1,22 @@
+CONNECT TO 'gestion_utu@miServidor' USER 'XXXNOMBREUSUSARIOXXX'  USING 'XXXPASSWORDXXX';
+
+INSERT INTO relacion_Alumno_Materias_Grupos (foranea_CI_alumno , foranea_id_materia , foranea_id_grupo )
+VALUES (13839024 , 1 , 1 );
+INSERT INTO relacion_Alumno_Materias_Grupos (foranea_CI_alumno , foranea_id_materia , foranea_id_grupo )
+VALUES (58296081 , 1 , 1 );
+INSERT INTO relacion_Alumno_Materias_Grupos (foranea_CI_alumno , foranea_id_materia , foranea_id_grupo )
+VALUES (53099404 , 1 , 1 );
+INSERT INTO relacion_Alumno_Materias_Grupos (foranea_CI_alumno , foranea_id_materia , foranea_id_grupo )
+VALUES (29293014 , 2 , 1 );
+INSERT INTO relacion_Alumno_Materias_Grupos (foranea_CI_alumno , foranea_id_materia , foranea_id_grupo )
+VALUES (29729647 , 2 , 1 );
+INSERT INTO relacion_Alumno_Materias_Grupos (foranea_CI_alumno , foranea_id_materia , foranea_id_grupo )
+VALUES (32481256 , 2 , 2 );
+INSERT INTO relacion_Alumno_Materias_Grupos (foranea_CI_alumno , foranea_id_materia , foranea_id_grupo )
+VALUES (54220963 , 2 , 2 );
+INSERT INTO relacion_Alumno_Materias_Grupos (foranea_CI_alumno , foranea_id_materia , foranea_id_grupo )
+VALUES (14303066 , 1 , 2 );
+INSERT INTO relacion_Alumno_Materias_Grupos (foranea_CI_alumno , foranea_id_materia , foranea_id_grupo )
+VALUES (57414907 , 1 , 2 );
+INSERT INTO relacion_Alumno_Materias_Grupos (foranea_CI_alumno , foranea_id_materia , foranea_id_grupo )
+VALUES (19988998 , 1 , 2 );
