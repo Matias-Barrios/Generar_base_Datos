e05f187a6ea2857d609daa68afd97a07e793760b
MatiasBarrios <soymatiasbarrios@gmail.com>
2018-05-12 00:01:44 -0300

    Leo

--- CrearDB/ejecutar_todo.sh
+++ CrearDB/ejecutar_todo.sh
@@ -3,12 +3,12 @@
 echo "Ejecutar todo - $( date )"
 
 dbaccess - Generar_estructuras.sql && \
-dbaccess - ingresar_alumnos_auto.sql && \
-dbaccess - ingresar_docentes_auto.sql && \
-dbaccess - ingresar_departamentos_auto.sql && \
-dbaccess - ingresar_institutos_auto.sql && \
-dbaccess - ingresar_materias_auto.sql && \
-dbaccess - ingresar_grupos_auto.sql
+dbaccess - LEO_ingresar_ciudades_auto.sql && \
+dbaccess - LEO_ingresar_materias_auto.sql  && \
+dbaccess - LEO_ingresar_institutos_auto.sql && \
+dbaccess - LEO_ingresar_grupos_auto.sql && \
+dbaccess - LEO_ingresar_personas_auto.sql
+
 
 echo "Completado!! "`date`
 
