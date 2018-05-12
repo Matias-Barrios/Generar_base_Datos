70201a5863407f540ff15d436775cb24fea6ca37
clferreri <38968565+clferreri@users.noreply.github.com>
2018-05-12 01:19:15 -0300

    Create relacion_Alumno_pertenece_Instituto.sql

---
+++ CrearDB/relacion_Alumno_pertenece_Instituto.sql
@@ -0,0 +1,20 @@
+INSERT INTO relacion_Alumno_pertenece_Instituto (foranea_CI_alumno, foranea_id_instituto)
+VALUES (13839024, 1);
+INSERT INTO relacion_Alumno_pertenece_Instituto (foranea_CI_alumno, foranea_id_instituto)
+VALUES (58296081, 1);
+INSERT INTO relacion_Alumno_pertenece_Instituto (foranea_CI_alumno, foranea_id_instituto)
+VALUES (53099404, 1);
+INSERT INTO relacion_Alumno_pertenece_Instituto (foranea_CI_alumno, foranea_id_instituto)
+VALUES (29293014, 1);
+INSERT INTO relacion_Alumno_pertenece_Instituto (foranea_CI_alumno, foranea_id_instituto)
+VALUES (29729647, 1);
+INSERT INTO relacion_Alumno_pertenece_Instituto (foranea_CI_alumno, foranea_id_instituto)
+VALUES (32481256, 1);
+INSERT INTO relacion_Alumno_pertenece_Instituto (foranea_CI_alumno, foranea_id_instituto)
+VALUES (54220963, 1);
+INSERT INTO relacion_Alumno_pertenece_Instituto (foranea_CI_alumno, foranea_id_instituto)
+VALUES (14303066, 1);
+INSERT INTO relacion_Alumno_pertenece_Instituto (foranea_CI_alumno, foranea_id_instituto)
+VALUES (57414907, 1);
+INSERT INTO relacion_Profesor_pertenece_Instituto (foranea_CI_profesor, foranea_id_instituto)
+VALUES (19988998, 1);
