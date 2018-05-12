ac6161a42419b33c55d43fea1506ee2b0ad071a8
Matias.Barrios <30472800+Matias-Barrios@users.noreply.github.com>
2018-05-12 12:12:42 -0300

    Update and rename Ingresa_Evaluacion.sql to LEO_ingresa_evaluaciones.sql

--- CrearDB/LEO_ingresa_evaluaciones.sql
+++ CrearDB/LEO_ingresa_evaluaciones.sql
@@ -1,4 +1,5 @@
-+CONNECT TO 'gestion_utu@miServidor' USER 'XXXNOMBREUSUSARIOXXX'  USING 'XXXPASSWORDXXX';
+CONNECT TO 'gestion_utu@miServidor' USER 'XXXNOMBREUSUSARIOXXX'  USING 'XXXPASSWORDXXX';
+
 INSERT INTO Evaluaciones (,CI_Profesor,CI_Alumno,id_Materia,id_Grupo,Evaluaciones_nombre_vacio,categoria,fecha_eva_vacio,baja,nota) VALUES
 (43053842,57414907,1,2,'Trabajo_domiciliario','01/4/2018','f',8)
 (19312327,13839024,1,1,'Trabajo_practico','01/05/2018','f',9)
