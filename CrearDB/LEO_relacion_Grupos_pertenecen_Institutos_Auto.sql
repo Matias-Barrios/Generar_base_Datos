f238f12517d6a518501c3d7f594c172d3a1c4f32
Matias.Barrios <30472800+Matias-Barrios@users.noreply.github.com>
2018-05-12 00:48:22 -0300

    Update LEO_relacion_Grupos_pertenecen_Institutos_Auto.sql

--- CrearDB/LEO_relacion_Grupos_pertenecen_Institutos_Auto.sql
+++ CrearDB/LEO_relacion_Grupos_pertenecen_Institutos_Auto.sql
@@ -1,4 +1,9 @@
 CONNECT TO 'gestion_utu@miServidor' USER 'XXXNOMBREUSUSARIOXXX'  USING 'XXXPASSWORDXXX';
 
-
+INSERT INTO relacion_Grupos_pertenece_Instituto (foranea_id_grupo, foranea_id_instituto)
+VALUES ( 1 , 1);
+INSERT INTO relacion_Grupos_pertenece_Instituto (foranea_id_grupo, foranea_id_instituto)
+VALUES ( 2 , 1);
+INSERT INTO relacion_Grupos_pertenece_Instituto (foranea_id_grupo, foranea_id_instituto)
+VALUES ( 3 , 1);
 
