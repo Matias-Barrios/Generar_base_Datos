da734d1dd3964a065df954e5acd6cc5e622db5aa
Matias.Barrios <30472800+Matias-Barrios@users.noreply.github.com>
2018-05-12 12:33:35 -0300

    Update eliminar_estructuras.sql

--- CrearDB/eliminar_estructuras.sql
+++ CrearDB/eliminar_estructuras.sql
@@ -5,16 +5,18 @@ CONNECT TO 'gestion_utu@miServidor' USER 'XXXNOMBREUSUSARIOXXX' USING 'XXXPASSWO
 ------------------------------------
 
 DROP TABLE IF EXISTS Personas;
-DROP TABLE IF EXISTS Grupos;
-DROP TABLE IF EXISTS Materias;
 DROP TABLE IF EXISTS Evaluaciones;
+DROP TABLE IF EXISTS Grupos;
 DROP TABLE IF EXISTS Institutos;
-DROP TABLE IF EXISTS Departamentos;
+DROP TABLE IF EXISTS Materias;
+DROP TABLE IF EXISTS Ciudad;
+DROP TABLE IF EXISTS Alumno;
+DROP TABLE IF EXISTS Profesor;
 
 
-DROP TABLE IF EXISTS relacion_personas_pertenecen_instituto;
-DROP TABLE IF EXISTS relacion_materias_pertenecen_grupos;
-DROP TABLE IF EXISTS relacion_ternaria_Personas_Grupos_Evaluaciones;
-DROP TABLE IF EXISTS relacion_Evaluaciones_pertenecen_Personas;
-DROP TABLE IF EXISTS relacion_personas_pertenecen_grupos;
-
+DROP TABLE IF EXISTS relacion_Profesor_pertenece_Instituto;
+DROP TABLE IF EXISTS relacion_Alumno_pertenece_Instituto;
+DROP TABLE IF EXISTS relacion_Grupos_pertenecen_Institutos;
+DROP TABLE IF EXISTS relacion_Grupos_tienen_Materias;
+DROP TABLE IF EXISTS relacion_Alumno_Materias_Grupos;
+DROP TABLE IF EXISTS relacion_Profesor_Materias_Grupos;
