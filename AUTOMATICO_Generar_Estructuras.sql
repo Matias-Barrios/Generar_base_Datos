155feabae7325b09511e7b321f6ef5303ab27a47
MatiasBarrios <soymatiasbarrios@gmail.com>
2018-05-13 16:08:08 -0300

    mas

---
+++ AUTOMATICO_Generar_Estructuras.sql
@@ -0,0 +1,177 @@
+CONNECT TO 'gestion_utu@miServidor' USER 'XXXNOMBREUSUSARIOXXX'  USING 'XXXPASSWORDXXX';
+
+
+
+-- ACA CREAMOS LAS TABLAS DE ENTIDAD
+------------------------------------
+
+DROP TABLE IF EXISTS Personas;
+DROP TABLE IF EXISTS Evaluaciones;
+DROP TABLE IF EXISTS Grupos;
+DROP TABLE IF EXISTS Institutos;
+DROP TABLE IF EXISTS Materias;
+DROP TABLE IF EXISTS Ciudad;
+DROP TABLE IF EXISTS Alumno;
+DROP TABLE IF EXISTS Profesor;
+
+CREATE TABLE Ciudad
+ (
+  id_ciudad SERIAL PRIMARY KEY  CONSTRAINT Ciudad_clave_primaria,
+  nombre_ciudad varchar(50) NOT NULL CONSTRAINT Ciudad_nombre_vacio,
+  nombre_departamento varchar(50) NOT NULL CONSTRAINT Departamento_nombre_vacio,
+  baja boolean NOT NULL CONSTRAINT Ciudad_baja_vacio
+ );
+
+ 
+ CREATE TABLE Materias
+ (
+  id_materia  SERIAL PRIMARY KEY  CONSTRAINT Materias_clave_primaria,
+  nombre_materia  varchar(25) NOT NULL CONSTRAINT Materias_nombre_not_null,
+  descripcion   varchar(255),
+  baja boolean NOT NULL CONSTRAINT Materias_baja_vacio
+ );
+
+CREATE TABLE Institutos
+ (
+  id_instituto  SERIAL PRIMARY KEY  CONSTRAINT Institutos_clave_primaria,
+  nombre  varchar(50) NOT NULL CONSTRAINT Institutos_nombre_vacio,
+  calle   varchar(50) NOT NULL CONSTRAINT Institutos_calle_vacio,
+  numero  INT,
+  telefonos varchar(100),
+  email varchar(80),
+  baja boolean NOT NULL CONSTRAINT Institutos_baja_vacio,
+
+  foranea_id_ciudad INTEGER REFERENCES Ciudad (id_ciudad) CONSTRAINT Institutos_fk_id_ciudad
+ ); 
+
+CREATE TABLE Grupos
+ (
+  id_grupo  SERIAL PRIMARY KEY  CONSTRAINT Grupos_clave_primaria,
+  nombre_grupo  varchar(5) NOT NULL CONSTRAINT Grupos_nombre_not_null,
+  orientacion   varchar(25) NOT NULL CHECK (orientacion IN ('ADMINISTRACIÓN','ELECTROELECTRÓNICA','QUÍMICA_BÁSICA','QUÍMICA_INDUSTRIAL','AERONÁUTICA','ELECTROMECÁNICA','TERMODINÁMICA','AGRARIO','ELECTROMECÁNICA_AUTOMOTRIZ','TURISMO','CONSTRUCCIÓN','INFORMÁTICA','DEPORTE_Y_RECREACIÓN','MAQUINISTA_NAVAL','ARTES_GRÁFICAS','ENERGÍAS_RENOVABLES','AUDIOVISUAL')) CONSTRAINT orientacion_valida,
+  turno   varchar(25) NOT NULL CHECK (turno IN ('Vespertino', 'Matutino', 'Nocturno')) CONSTRAINT turno_valido,
+  baja boolean NOT NULL CONSTRAINT Grupos_baja_vacio,
+
+  foranea_id_instituto INTEGER REFERENCES Institutos (id_instituto) CONSTRAINT Grupos_fk_id_instituto
+ );
+
+CREATE TABLE Personas
+ (
+  CI  INT PRIMARY KEY  CONSTRAINT Personas_clave_primaria,
+  primer_nombre   varchar(25) NOT NULL CONSTRAINT primer_nombre_vacio,
+  segundo_nombre   varchar(25),
+  primer_apellido   varchar(25) NOT NULL CONSTRAINT primer_apellido_vacio,
+  segundo_apellido   varchar(25),
+  fecha_nacimiento DATE NOT NULL CONSTRAINT fecha_nacimiento_vacio,
+  email varchar(80),
+  encriptacion_hash varchar(250),
+  encriptacion_sal varchar(250),
+  baja boolean NOT NULL CONSTRAINT Personas_baja_vacio
+ );
+
+ CREATE TABLE Alumno
+ (
+  CI_alumno INT PRIMARY KEY REFERENCES Personas (CI) CONSTRAINT Alumno_fk_personas_CI,
+  hace_proyecto boolean NOT NULL CONSTRAINT hace_proyecto_vacio,
+  nota_final INT CHECK ( nota_final > 0 AND nota_final < 13) CONSTRAINT nota_final_valida,
+  baja boolean NOT NULL CONSTRAINT Alumno_baja_vacio
+ );
+
+ CREATE TABLE Profesor
+ (
+  CI_profesor INT PRIMARY KEY REFERENCES Personas (CI) CONSTRAINT Profesor_fk_personas_CI,
+  grado INT CHECK ( grado > 0 AND grado < 8) CONSTRAINT grado_valido,
+  baja boolean NOT NULL CONSTRAINT Profesor_baja_vacio
+ );
+            
+ 
+ CREATE TABLE Evaluaciones
+ (
+  id_evaluacion  SERIAL,
+  CI_profesor INTEGER REFERENCES Personas (CI) CONSTRAINT evaluaciones_fk_personas_CI_profesor,
+  CI_alumno INTEGER REFERENCES Personas (CI) CONSTRAINT evaluaciones_fk_personas_CI_alumno,
+  id_materia INTEGER REFERENCES Materias (id_materia) CONSTRAINT evaluaciones_fk_materias_id_materia,
+  id_grupo INTEGER REFERENCES Grupos (id_grupo) CONSTRAINT evaluaciones_fk_grupos_id_grupo,
+  nombre_evaluacion varchar(40) NOT NULL CONSTRAINT Evaluaciones_nombre_vacio,
+  categoria varchar(30) NOT NULL CHECK (categoria IN ('Trabajo_laboratorio', 'Trabajo_domiciliario', 'Trabajo_practico', 'Trabajo_investigacion', 'Trabajo_escrito', 'Oral', 'Parcial', 'Primera_entrega_proyecto', 'Segunda_entrega_proyecto', 'Tercera_entrega_proyecto', 'Defensa_individual', 'Defensa_grupal')) CONSTRAINT Evaluaciones_categoria_valida,
+  fecha_eva DATE NOT NULL CONSTRAINT fecha_eva_vacio,
+  descripcion   varchar(255),
+  nota INT CHECK ( nota > 0 AND nota < 13) CONSTRAINT evaluaciones_nota_valida,
+  baja boolean NOT NULL CONSTRAINT Evaluaciones_baja_vacio,
+
+  PRIMARY KEY (id_evaluacion,CI_profesor, CI_alumno, id_materia, id_grupo ) CONSTRAINT evaluaciones_clave_primaria
+ );
+
+
+
+
+                                        
+                                        
+-- ACA CREAMOS LAS TABLAS DE RELACIONES
+------------------------------------ nota INT CHECK ( nota > 0 AND nota < 13) CONSTRAINT nota_valida,
+-- grado INT CHECK ( grado > 0 AND grado < 8) CONSTRAINT validar_grado,
+-- tipo varchar(20) CHECK (tipo IN ('Admin', 'Docente', 'Alumno')) CONSTRAINT tipo_valido,
+
+
+DROP TABLE IF EXISTS relacion_Grupos_tienen_Materias;
+DROP TABLE IF EXISTS relacion_Grupos_pertenecen_Institutos;
+DROP TABLE IF EXISTS relacion_Profesor_pertenece_Instituto;
+DROP TABLE IF EXISTS relacion_Alumno_pertenece_Instituto;
+DROP TABLE IF EXISTS relacion_Alumno_Materias_Grupos;
+
+DROP TABLE IF EXISTS relacion_Profesor_Materias_Grupos;
+
+
+
+                                        
+CREATE TABLE relacion_Profesor_pertenece_Instituto
+(
+    foranea_CI_profesor INTEGER REFERENCES Profesor (CI_profesor) CONSTRAINT relacion_Profesor_pertenece_Instituto_fk_Personas_CI,
+    foranea_id_instituto  INTEGER REFERENCES Institutos (id_instituto) CONSTRAINT relacion_Profesor_pertenece_Instituto_fk_id_instituto,
+    PRIMARY KEY (foranea_CI_profesor, foranea_id_instituto) CONSTRAINT relacion_Profesor_pertenece_Instituto_clave_primaria
+);
+
+CREATE TABLE relacion_Alumno_pertenece_Instituto
+(
+    foranea_CI_alumno INTEGER REFERENCES Personas (CI) CONSTRAINT relacion_Alumno_pertenece_Instituto_fk_Personas_CI,
+    foranea_id_instituto  INTEGER REFERENCES Institutos (id_instituto) CONSTRAINT relacion_Alumno_pertenece_Instituto_fk_id_instituto,
+    PRIMARY KEY (foranea_CI_alumno, foranea_id_instituto) CONSTRAINT relacion_Alumno_pertenece_Instituto_clave_primaria
+);
+
+
+CREATE TABLE relacion_Grupos_pertenecen_Institutos
+(
+    foranea_id_grupo INTEGER REFERENCES Grupos (id_grupo) CONSTRAINT relacion_Grupos_pertenecen_Institutos_fk_Grupos_id_grupo,
+    foranea_id_instituto  INTEGER REFERENCES Institutos (id_instituto) CONSTRAINT relacion_Grupos_pertenecen_Institutos_fk_id_instituto,
+    PRIMARY KEY (foranea_id_grupo, foranea_id_instituto) CONSTRAINT relacion_Grupos_pertenecen_Institutos_clave_primaria
+);
+
+CREATE TABLE relacion_Grupos_tienen_Materias
+(
+    foranea_id_grupo INTEGER REFERENCES Grupos (id_grupo) CONSTRAINT relacion_Grupos_tienen_Materias_fk_Grupos_id_grupo,
+    foranea_id_materia  INTEGER REFERENCES Materias (id_materia) CONSTRAINT relacion_Grupos_tienen_Materias_fk_id_materia,
+    PRIMARY KEY (foranea_id_grupo,foranea_id_materia) CONSTRAINT relacion_Grupos_tienen_Materias_clave_primaria
+);
+
+CREATE TABLE relacion_Alumno_Materias_Grupos
+(
+    foranea_CI_alumno INTEGER REFERENCES Personas (CI) CONSTRAINT relacion_Alumno_Materias_Grupos_fk_Personas_CI,
+    foranea_id_materia  INTEGER REFERENCES Materias (id_materia) CONSTRAINT relacion_Alumno_Materias_Grupos_fk_id_materia,
+    foranea_id_grupo INTEGER REFERENCES Grupos (id_grupo) CONSTRAINT relacion_Alumno_Materias_Grupos_fk_id_grupo,
+    PRIMARY KEY (foranea_CI_alumno, foranea_id_materia, foranea_id_grupo) CONSTRAINT rrelacion_personas_pertenecen_grupos_clave_primaria
+);
+
+CREATE TABLE relacion_Profesor_Materias_Grupos
+(
+    foranea_CI_profesor INTEGER REFERENCES Profesor (CI_profesor) CONSTRAINT relacion_Profesor_Materias_Grupos_fk_Personas_CI,
+    foranea_id_materia  INTEGER REFERENCES Materias (id_materia) CONSTRAINT relacion_Profesor_Materias_Grupos_fk_id_materia,
+    foranea_id_grupo INTEGER REFERENCES Grupos (id_grupo) CONSTRAINT relacion_Profesor_Materias_Grupos_fk_id_grupo,
+    PRIMARY KEY (foranea_CI_profesor, foranea_id_materia, foranea_id_grupo) CONSTRAINT relacion_Profesor_Materias_Grupos_clave_primaria
+);
+
+
+
+
+-- 
+                                        
+                                        
---
+++ AUTOMATICO_ingresar_institutos_auto.sql
@@ -0,0 +1,52 @@
+CONNECT TO 'gestion_utu@miServidor' USER 'XXXNOMBREUSUSARIOXXX' USING 'XXXPASSWORDXXX';
+
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja, foranea_id_ciudad)
+VALUES ('Escuela Técnica "Arroyo Seco"', "Av. Agraciada Esq. Aguilar", 2544, "29243865|29243856", "etas010@gmail.com", "f", 2);
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja, foranea_id_ciudad)
+VALUES ('Intituto Técnico Superior "Buceo"', "Av. Rivera Esq. Tomas de Tésanos", 3729 , "26285408|26285813", "itsbuceo@gmail.com", "f", 2);
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja, foranea_id_ciudad)
+VALUES ('Escuela Técnica Cerro "Mtro. Nicasio García"', "Portugal Esq. Carlos Mª Ramírez", 4257 , "23111056|23119407|23114949", "portugal4257@hotmail.com", "f", 2);
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja, foranea_id_ciudad)
+VALUES ('Escuela Técnica Colón  "Don Alberico Passadore"', "Cno. Colman Esq. Cesar Mayo Gutierrez", 5274 , "23209511|23205789", "estecolon@gmail.com", "f", 2);
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja, foranea_id_ciudad)
+VALUES ('Escuela Técnica Flor de Maroñas', "Andrés Latorre Esq. Veracierto", 4914 , "25148177|25148210", "utuflor2012@hotmail.com", "f", 2);
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja, foranea_id_ciudad)
+VALUES ('Escuela Técnica Artigas', "Bernabé Rivera Esq. Gral Rivera", 626 , "47723687|47725988", "etautu626@adinet.com.uy", "f", 1);
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja, foranea_id_ciudad)
+VALUES ('Escuela Técnica de Atlántida', "Calle Republica Argentina Entre Republica de Chile y Gral. Artigas", NULL, "43723507|43728578", "utuatlantida@adinet.com.uy", "f", 2);
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja, foranea_id_ciudad)
+VALUES ('Escuela Técnica Las Piedras', "Baltasar Brum Esq. Pilar Cabrera", 665, "23644564|23641741|23649849", "escuelalaspiedras2011@gmail.com", "f", 2);
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja, foranea_id_ciudad)
+VALUES ('Escuela Técnica San Ramón "Juan Pedro Tapie"', "José Batlle Y Ordóñez Esq. Rivera", 1698, "43122409|43123593", "utujpt@adinet.com.uy", "f", 2);
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja, foranea_id_ciudad)
+VALUES ('Escuela Técnica Fraile Muerto', "Julio María Cremona s/n Esq. 25 de agosto.(Fraile Muerto)", NULL, "46889031", "escuelatecnicafrailemuertocl@gmail.com", "f", 3);
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja, foranea_id_ciudad)
+VALUES ('Escuela Técnica Melo', "Treinta y Tres Esq. Oribe (Melo)", 290, "46422121|46429133|46424496", "escuelatmelo@gmail.com", "f", 3);
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja, foranea_id_ciudad)
+VALUES ('Escuela Técnica Carmelo', "José E. Rodó Esq. Ruta 21", NULL, "45422317|45424015", "utucarmelo@hotmail.com", "f", 2);
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja, foranea_id_ciudad)
+VALUES ('Escuela Técnica ERCNA', "18 de Julio s/n esq. Arroyo de las Vacas (Carmelo)", NULL, "45422372|45423912", "utu_ercna@hotmail.com", "f", 2);
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja, foranea_id_ciudad)
+VALUES ('Escuela Técnica Colonia', "F.D. Roosvelt esq. Río Negro", 167, "45222960|45220261", "utu.colonia.sac@gmail.com", "f", 2);
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja, foranea_id_ciudad)
+VALUES ('Escuela Técnica Durazno', "Herrera Esq. Juan Bautista de León", 1098, "43626288|43627735", "etecnicadurazno@gmail.com", "f", 2);
+
+INSERT INTO Institutos (nombre, calle, numero, telefonos, email, baja, foranea_id_ciudad)
+VALUES ('Escuela Técnica Sarandí del Yí', "A. Sosa Bernadet Esq. Alvariza", 579, "43679068|43639099", "utusarandidelyi@gmail.com", "f", 2);
+
+-- fin
